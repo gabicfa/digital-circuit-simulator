d94a4cc4e83ad5f80995d0127d3370ab160241e2
gabicfa <gabrielacfa@al.insper.edu.br>
2016-03-30 00:53:53 -0300

    Projeto 2

--- src/AndGate.java
+++ src/AndGate.java
@@ -1,4 +1,19 @@
 
 public class AndGate extends LogicGate {
-
+	private InputPin pinA;
+    private InputPin pinB;
+    /* ...algum código... */
+    // Este método devolve o sinal de um pino de saída da porta,
+    // indicado pelo índice index.
+    @Override
+    public boolean getOutputValue(int index) {
+      // Como uma porta AND possui apenas um pino de saída, vamos
+      // simplesmente supor que index é zero e ignorá-lo. Ele
+      // poderia ser outros valores para portas que possuem
+      // múltiplos pinos de saída, como um adder.
+      boolean sinalA = pinA.getSource().getOutputValue(pinA.getIndex());
+      boolean sinalB = pinB.getSource().getOutputValue(pinB.getIndex());
+      return sinalA && sinal B;
+    }
+    /* ...algum código... */
 }
--- src/Switch.java
+++ src/Switch.java
@@ -1,3 +1,12 @@
 public class Switch implements Source {
-
+	private boolean botao;
+	
+	public void ligarBotao(){
+		botao = true;
+	}
+	
+	public void desligarBotao(){
+		botao = false;
+	}
+	
 }
