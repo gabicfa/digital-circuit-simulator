22d7cf905280773c2bf930fe1007bdb5c5be27d5
Otofuji <ericfoa@al.insper.edu.br>
2016-04-01 09:45:10 -0300

    Inicializando a visão

---
+++ src/visao/Visao.java
@@ -0,0 +1,35 @@
+package visao; //e agora?
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Visao {
+	public static void main(String s[]) {
+		JFrame frame = new JFrame ("Simulador de Circuito Digital");
+		JPanel panel = newJPanel();
+		panel.setLayout(new FlowLayout());;
+		JLabel label = new JLabel("Selecione a porta lógica que deseja simular");
+		JButton not = new JButton();
+		not.setText("Not");
+		JButton and = new JButton();
+		and.setText("And");
+		JButton or = new JButton();
+		or.setText("Or");
+		JButton xor = new JButton();
+		xor.setText("Xor");
+		panel.add(label);
+		panel.add(not);
+		panel.add(and);
+		panel.add(or);
+		panel.add(xor);
+		frame.add(panel);
+		frame.setSize(500, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
