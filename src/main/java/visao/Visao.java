f0ad0d4939d1a8bd26a147f12857c8b9e76355ac
Otofuji <ericfoa@al.insper.edu.br>
2016-04-04 10:35:28 -0300

    Adicionando JCheckBox
    NOTA: devido a um bug com o Eclipse, não estou conseguindo rodar o
    programa desde os últimos dois commits, então por enquanto estou
    programando “às cegas” sem testar o programa. Por isso, potencialmente
    o código atual não funciona. Assim que terminar de estruturar o código,
    darei um jeito de resolver o bug para corrigir.

--- src/main/java/visao/Visao.java
+++ src/main/java/visao/Visao.java
@@ -3,12 +3,14 @@ package visao;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JCheckBox;
 
 public class Visao {
 	public static void main(String s[]) {
@@ -27,6 +29,13 @@ public class Visao {
 				notWindow.setSize(500,300); //bug não funciona
 				notWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				notWindow.setVisible(true);
+				private JCheckBox notA, notB;
+				notA = new JCheckBox("A");
+				notB = new JCheckBox("B");
+				add(notA);
+				add(notB);
+				setVisible(true);
+				
 				public class NotGateDesenho extends GateDesenho{
 				    @Override
 				    protected void paintComponent(Graphics g) {
@@ -54,6 +63,13 @@ public class Visao {
 				andWindow.setSize(500,300); //bug não funciona
 				andWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				andWindow.setVisible(true);
+				private JCheckBox andA, andB;
+				andA = new JCheckBox("A");
+				andB = new JCheckBox("B");
+				add(andA);
+				add(andB);
+				setVisible(true);
+				
 				public class AndGateDesenho extends GateDesenho{ 
 
 				    @Override
@@ -82,6 +98,14 @@ public class Visao {
 				orWindow.setSize(500,300); //bug não funciona
 				orWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				orWindow.setVisible(true);
+				private JCheckBox orA, orB;
+				orA = new JCheckBox("A");
+				orB = new JCheckBox("B");
+				add(orA);
+				add(orB);
+				setVisible(true);
+				
+				public class OrGateDesenho extends GateDesenho{
 			    @Override
 			    protected void paintComponent(Graphics g) {
 			        int x = 160;
@@ -108,6 +132,12 @@ public class Visao {
 				xorWindow.setSize(500,300); //bug não funciona
 				xorWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				xorWindow.setVisible(true);
+				private JCheckBox xorA, xorB;
+				xorA = new JCheckBox("A");
+				xorB = new JCheckBox("B");
+				add(xorA);
+				add(xorB);
+				setVisible(true);
 				public class XorGateDesenho extends GateDesenho{
 					
 				    @Override
@@ -137,6 +167,13 @@ public class Visao {
 				nandWindow.setSize(500,300); //bug não funciona
 				nandWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				nandWindow.setVisible(true);
+				private JCheckBox nandA, nandB;
+				nandA = new JCheckBox("A");
+				nandB = new JCheckBox("B");
+				add(nandA);
+				add(nandB);
+				setVisible(true);
+				
 				public class NandGateDesenho extends GateDesenho{
 					
 				    public void paintComponent(Graphics g) {
@@ -166,8 +203,17 @@ public class Visao {
 				fullAdderWindow.setSize(500,300); //bug não funciona
 				fullAdderWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				fullAdderWindow.setVisible(true);
+				private JCheckBox fullAdderA, fullAdderB, fullAdderC;
+				fullAdderA = new JCheckBox("A");
+				fullAdderB = new JCheckBox("B");
+				fullAdderC = new JCheckBox("C");
+				add(fullAdderA);
+				add(fullAdderB);
+				add(fullAdderC);
+				setVisible(true);
+				
 				public class FullAdderDesenho extends GateDesenho{
-					
+				
 				    @Override
 				    protected void paintComponent(Graphics g) {
 				        super.paintComponent(g);
@@ -199,6 +245,13 @@ public class Visao {
 				halfAdderWindow.setSize(500,300); //bug não funciona
 				halfAdderWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				halfAdderWindow.setVisible(true);
+				private JCheckBox halfAdderA, halfAdderB;
+				halfAdderA = new JCheckBox("A");
+				halfAdderB = new JCheckBox("B");
+				add(halfAdderA);
+				add(halfAdderB);
+				setVisible(True);
+				
 				public class HalfAdderDesenho extends GateDesenho{
 					
 				    @Override
